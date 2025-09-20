a33465e022b2e03fc9ed4ced4b8f0d66446b9421
Raafx <ahmadrafiin467@gmail.com>
2025-09-20 22:36:52 +0800

    add python program

---
+++ post-test/post-test-apd-2/2509106034-AHMAD RAFI' IRSYAD NUGRAHA-PT-2.py
@@ -0,0 +1,28 @@
+#Membuat List
+list_data = [173,70.5,"Rafi",True]
+
+#Menampilkan tipe data masing-masing elemen dalam list sebelum diubah
+print("Tipe data sebelum diubah: ")
+for i in list_data:
+    print(i,":",type(i))
+
+#Menampilkan jumlah data yang bertipe integer dan float sebelum diubah
+jumlah_int_dan_float = list_data[0]+list_data[1]
+print("jumlah data int dan float:",jumlah_int_dan_float)
+
+#Mengubah tipe data masing-masing element dalam list
+list_data[0] = str(list_data[0])
+list_data[1] = int(list_data[1])
+list_data[2] = list(list_data[2])
+list_data[3] = float(list_data[3])
+
+#Menampilkan tipe data masing-masing elemen dalam list  setelah diubah
+print("\nTipe data setelah diubah: ")
+for i in list_data:
+    print(i,":",type(i))
+
+#Menampilkan jumlah data yang bertipe integer dan float sebelum diubah
+jumlah_int_dan_float = list_data[1]+list_data[3]
+print("jumlah data int dan float:",jumlah_int_dan_float)
+
+    
