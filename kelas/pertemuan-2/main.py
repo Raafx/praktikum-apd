3e3f0d412836a21f699f88a138f804692939dc3a
Raafx <ahmadrafiin467@gmail.com>
2025-09-27 20:42:51 +0800

    add pertemuan-2

---
+++ kelas/pertemuan-2/main.py
@@ -0,0 +1,69 @@
+# print("hello world")
+
+
+# #tipe data string
+# nama_lengkap = "Ahmad Rafi"
+# print(nama_lengkap)
+
+# #tipe data integer
+# angka = 123
+# print(angka)
+
+# print ("""Halo,
+# Saya sedang belajar ngoding
+# Bahasa Pemrograman python""")
+
+# print("saya"+"daffa")
+
+
+
+# # menampilkan index 0 hingga 4
+# print(nama_lengkap[0:5])
+
+# # tipe data boolean
+# hujan = True
+# panas = False
+# print(hujan and panas) #False
+# print(hujan or panas) #True
+
+# #list
+# prodi = ['Informatika', 'Sistem Informasi', 'Teknik Sipil']
+# print(prodi[0])
+# print(prodi[1])
+# print(prodi[2])
+
+
+# #dictionary
+# biodata = {
+#     "nama":"dafa",
+#     "kelas":"B",
+#     "umur":19,
+#     2:10
+# }
+
+# #menampilkan masing masing value dari dictionary berdasarkan key-nya
+# print(biodata["nama"])
+# print(biodata["kelas"])
+# print(biodata["umur"])
+# print(biodata[2])
+
+# #menampilkan tipe data
+# print(type(biodata))
+# print(type(nama_lengkap))
+
+# #mengambil input manual dari user
+# nama = input("masukan nama anda: ") 
+# print(nama)
+
+# print(not 1>5)
+
+#studi kasus 1
+
+
+
+#studi kasus 2
+total_belanja = int(input("Masukan total belanja: "))
+pajak = 11/100
+
+hasil = total_belanja+(total_belanja*pajak)
+print(hasil)
