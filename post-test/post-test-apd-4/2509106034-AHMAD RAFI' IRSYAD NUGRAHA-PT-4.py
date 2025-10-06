25126db3ad46affd127434de5800e3980b9a6805
Raafx <ahmadrafiin467@gmail.com>
2025-10-06 11:34:09 +0800

    refactor looping structure

--- post-test/post-test-apd-4/2509106034-AHMAD RAFI' IRSYAD NUGRAHA-PT-4.py
+++ post-test/post-test-apd-4/2509106034-AHMAD RAFI' IRSYAD NUGRAHA-PT-4.py
@@ -1,73 +1,82 @@
+import os
+
+os.system("cls")
+
 #menginisiasi username dan password
 username = "Rafi"
 password = "034"
 
 #menginisiasi jumlah percobaan
-percobaan = 1
+percobaan = 0
 
 print("\n============Silahkan Login terlebih dahulu============\n")
-while True:
+
+#memasuki while looop selama jumlah percobaan kurang dari 5
+while percobaan < 5:
 
     #Menerima input username dan password dari user
     username_input = input("Masukan Username Anda: ")
     password_input = input("Masukan Password Anda: ")
 
-    #mengecek jumlah percobaan terlebih dahulu (nilai awal = 1)
-    if percobaan < 5:
+    #mengecek jumlah percobaan terlebih dahulu (nilai awal = 0)
 
-        #mengecek apakah username dan password yang diinput user sesuai dengan yang dinisiasi sebelumnya
-        if username_input == username and password_input == password:
-            print("\n=====Login Berhasil!=====\n")
-            login_status = True
+    #mengecek apakah username dan password yang diinput user sesuai dengan yang dinisiasi sebelumnya
+    #jika sesuai, maka login dinyatakan berhasil, dan memasuki kode program post-test sebelumnya
+    if username_input == username and password_input == password: 
+        login_status = True
+        print("\n=====Login Berhasil!=====\n")
+        break
+        
+         
 
-            while login_status: 
+    #jika tidak, maka jumlah percobaan akan bertambah 1
+    #Jika jumlah percobaan mencapai 5, while loop akan berhenti
+    else:
+        print("Username dan Password tidak sesuai, coba lagi!")
+        print("=============================================")
+        percobaan+=1
+        login_status = False
+    
 
+while login_status:
+            #menginput ketiga sisi segitiga dari user
+        sisi_A = int(input("Masukan sisi pertama: "))
+        sisi_B = int(input("Masukan sisi kedua: "))
+        sisi_C = int(input("Masukan sisi ketiga: "))
 
-                #menginput ketiga sisi segitiga dari user
+        #Menentukan jenis segitiga berdasarkan panjang masing-masing sisinya
+        if sisi_A <= 0 or sisi_B <= 0 or sisi_C <= 0:
+            hasil = "Sisi tidak boleh bernilai kurang dari sama dengan 0!"
+        elif sisi_A+sisi_B <= sisi_C or sisi_B+sisi_C <= sisi_A or sisi_A+sisi_C <= sisi_B:
+            hasil = "Bukan Segitiga"
+        elif sisi_A == sisi_B == sisi_C:
+            hasil = "Segitiga sama sisi"
+        elif sisi_A == sisi_B != sisi_C or sisi_B == sisi_C != sisi_A or sisi_A == sisi_C != sisi_B:
+            hasil = "Segitiga sama kaki"
+        elif sisi_A != sisi_B != sisi_C:
+            hasil = "Segitiga sembarang"
 
-                sisi_A = int(input("Masukan sisi pertama: "))
-                sisi_B = int(input("Masukan sisi kedua: "))
-                sisi_C = int(input("Masukan sisi ketiga: "))
+        #Menampilkan jenis segitiga
+        print(hasil)
 
-                #Menentukan jenis segitiga berdasarkan panjang masing-masing sisinya
-                if sisi_A <= 0 or sisi_B <= 0 or sisi_C <= 0:
-                    hasil = "Sisi tidak boleh bernilai kurang dari sama dengan 0!"
-                elif sisi_A+sisi_B <= sisi_C or sisi_B+sisi_C <= sisi_A or sisi_A+sisi_C <= sisi_B:
-                    hasil = "Bukan Segitiga"
-                elif sisi_A == sisi_B == sisi_C:
-                    hasil = "Segitiga sama sisi"
-                elif sisi_A == sisi_B != sisi_C or sisi_B == sisi_C != sisi_A or sisi_A == sisi_C != sisi_B:
-                    hasil = "Segitiga sama kaki"
-                elif sisi_A != sisi_B != sisi_C:
-                    hasil = "Segitiga sembarang"
+        #Menghitung luas segitiga apabila terdefinisi sebagai segitiga
+        if hasil != "Bukan Segitiga" and hasil != "Sisi tidak boleh bernilai kurang dari sama dengan 0!":
+           s = 1/2*(sisi_A+sisi_B+sisi_C)
+           luas_segiitiga = (s*(s-sisi_A)*(s-sisi_B)*(s-sisi_C))**0.5
+           print("Luas Segitiga:",luas_segiitiga)
 
-                #Menampilkan jenis segitiga
-                print(hasil)
+        #Menanyakan user apakah ingin melanjutkan program
+        isLanjut = input("Apakah anda ingin mengulang program? [ya/tidak]: ")
 
-                #Menghitung luas segitiga apabila terdefinisi sebagai segitiga
-                if hasil != "Bukan Segitiga" and hasil != "Sisi tidak boleh bernilai kurang dari sama dengan 0!":
-                    s = 1/2*(sisi_A+sisi_B+sisi_C)
-                    luas_segiitiga = (s*(s-sisi_A)*(s-sisi_B)*(s-sisi_C))**0.5
-                    print("Luas Segitiga:",luas_segiitiga)
-
-                isLanjut = input("Apakah anda ingin mengulang program? [ya/tidak]: ")
-
-                if isLanjut != "ya":
-                    login_status = False
-
+        #Jika tidak, maka while akan berhenti dengan menggunakan break
+        if isLanjut != "ya":
             break
 
 
-    
-        else:
-            print("Username dan Passowrd tidak sesuai, coba lagi!")
-            print("=============================================")
-            percobaan+=1
-    else:
-        print("Anda terlalu banyak mencoba, program akan berhenti secara otomatis")
-        break
 
-
+#menampilkan pesan jika user mencoba login sebanyak 5 kali
+if percobaan == 5:
+    print("Anda terlalu banyak mencoba, program akan berhenti secara otomatis")
         
 
 print("\n===============Program Selesai===============\n")
