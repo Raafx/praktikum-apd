c8e835600d4c8921be1bf7ed4e9a504b90cfd5eb
Raafx <ahmadrafiin467@gmail.com>
2025-10-06 22:09:50 +0800

    refactor code structure

--- post-test/post-test-apd-4/2509106034-AHMAD RAFI' IRSYAD NUGRAHA-PT-4.py
+++ post-test/post-test-apd-4/2509106034-AHMAD RAFI' IRSYAD NUGRAHA-PT-4.py
@@ -1,49 +1,40 @@
+# Membersihkan terminal menggunakan import os
 import os
-
 os.system("cls")
 
 #menginisiasi username dan password
 username = "Rafi"
 password = "034"
 
-#menginisiasi jumlah percobaan
+#menginisiasi jumlah percobaan login
 percobaan = 0
 
 print("\n============Silahkan Login terlebih dahulu============\n")
 
-#memasuki while looop selama jumlah percobaan kurang dari 5
+#Menginisiasi status login, jika belum maka bernilai False
+login_status = False
+
+#memasuki while lop selama jumlah percobaan kurang dari 5
 while percobaan < 5:
 
-    #Menerima input username dan password dari user
-    username_input = input("Masukan Username Anda: ")
-    password_input = input("Masukan Password Anda: ")
-
-    #mengecek jumlah percobaan terlebih dahulu (nilai awal = 0)
+    #mengecek status login, jika belum (False) makan user diminta untuk menginput username dan password yang sesuai
+    #block code ini berfungsi untuk mencegah program mengulang lagi dari bagian login jika user ingin mengulang program
+    if login_status == False:
+        username_input = input("Masukan Username Anda: ")
+        password_input = input("Masukan Password Anda: ")
 
     #mengecek apakah username dan password yang diinput user sesuai dengan yang dinisiasi sebelumnya
     #jika sesuai, maka login dinyatakan berhasil, dan memasuki kode program post-test sebelumnya
-    if username_input == username and password_input == password: 
-        login_status = True
+    if username_input == username and password_input == password:
         print("\n=====Login Berhasil!=====\n")
-        break
-        
-         
 
-    #jika tidak, maka jumlah percobaan akan bertambah 1
-    #Jika jumlah percobaan mencapai 5, while loop akan berhenti
-    else:
-        print("Username dan Password tidak sesuai, coba lagi!")
-        print("=============================================")
-        percobaan+=1
-        login_status = False
+        #mengubah status login menjadi True, agar tidak mengulang lagi di bagian login
+        login_status = True
     
-
-while login_status:
-            #menginput ketiga sisi segitiga dari user
+        #menginput ketiga sisi segitiga dari user
         sisi_A = int(input("Masukan sisi pertama: "))
         sisi_B = int(input("Masukan sisi kedua: "))
         sisi_C = int(input("Masukan sisi ketiga: "))
-
         #Menentukan jenis segitiga berdasarkan panjang masing-masing sisinya
         if sisi_A <= 0 or sisi_B <= 0 or sisi_C <= 0:
             hasil = "Sisi tidak boleh bernilai kurang dari sama dengan 0!"
@@ -55,24 +46,28 @@ while login_status:
             hasil = "Segitiga sama kaki"
         elif sisi_A != sisi_B != sisi_C:
             hasil = "Segitiga sembarang"
-
         #Menampilkan jenis segitiga
         print(hasil)
-
         #Menghitung luas segitiga apabila terdefinisi sebagai segitiga
         if hasil != "Bukan Segitiga" and hasil != "Sisi tidak boleh bernilai kurang dari sama dengan 0!":
            s = 1/2*(sisi_A+sisi_B+sisi_C)
            luas_segiitiga = (s*(s-sisi_A)*(s-sisi_B)*(s-sisi_C))**0.5
            print("Luas Segitiga:",luas_segiitiga)
-
         #Menanyakan user apakah ingin melanjutkan program
         isLanjut = input("Apakah anda ingin mengulang program? [ya/tidak]: ")
-
-        #Jika tidak, maka while akan berhenti dengan menggunakan break
-        if isLanjut != "ya":
+        if isLanjut == "ya":
+            continue
+        else: 
             break
+    
+    #jika username dan password yang diinput user tidak sesuai, maka jumlah percobaan akan bertambah 1
+    #Jika jumlah percobaan mencapai 5, while loop akan berhenti
 
-
+    else:
+        print("Username dan Password tidak sesuai, coba lagi!")
+        print("=============================================")
+        percobaan+=1
+   
 
 #menampilkan pesan jika user mencoba login sebanyak 5 kali
 if percobaan == 5:
