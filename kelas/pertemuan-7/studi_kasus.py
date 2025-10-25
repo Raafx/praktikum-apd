de89e37f4c4afa085630ab49d1d37701cd9e38ab
Raafx <ahmadrafiin467@gmail.com>
2025-10-25 14:31:35 +0800

    add post-test-7

--- kelas/pertemuan-7/studi_kasus.py
+++ kelas/pertemuan-7/studi_kasus.py
@@ -1,23 +1,27 @@
-# while True:
-#     try:
-#         nama = input("Masukan Nama Anda: ")
+# # while True:
+# #     try:
+# #         nama = input("Masukan Nama Anda: ")
         
-#         if nama == "" or nama == " "*len(nama):
-#             raise ValueError("Nama tidak boleh kosong")
-#         else:
-#             print("Nama Anda: ")
-#     except ValueError as e:
-#         print(e)
+# #         if nama == "" or nama == " "*len(nama):
+# #             raise ValueError("Nama tidak boleh kosong")
+# #         else:
+# #             print("Nama Anda: ")
+# #     except ValueError as e:
+# #         print(e)
 
-daftar_akun = {
-    "bakil":"admin123",
-    "jopan": "user123",
-    "rafi":"rafi467"
-}
+# daftar_akun = {
+#     "bakil":"admin123",
+#     "jopan": "user123",
+#     "rafi":"rafi467"
+# }
 
-try:
-    usernmae = input("Masukan Username untuk melihat password anda")
-    pasword = daftar_akun[usernmae]
-    print(pasword)
-except ValueError:
-    print("Akun tidak ditemukan")
+# try:
+#     usernmae = input("Masukan Username untuk melihat password anda")
+#     pasword = daftar_akun[usernmae]
+#     print(pasword)
+# except ValueError:
+#     print("Akun tidak ditemukan")
+    
+# if False:
+    
+        
